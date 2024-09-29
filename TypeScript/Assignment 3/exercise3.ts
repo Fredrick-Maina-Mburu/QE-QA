f6e4a8faa2a9c51490d03a4463786da6015443aa
FredrickMburu <109967236+FredrickMburu@users.noreply.github.com>
2024-09-29 13:30:26 +0300

    reducing duplication with utility types

--- TypeScript/Assignment 3/exercise3.ts
+++ TypeScript/Assignment 3/exercise3.ts
@@ -30,3 +30,42 @@ type RequiredAlbum = Required<Albums>;
 
 const updateAlbums = (album: RequiredAlbum) => {}
 
+// Exercises
+// Exercise 1: Expecting Certain Properties
+
+interface User1 {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+type NameAndEmail = Pick<User1, 'id' | 'name'>
+
+const fetchUser = async (): Promise<NameAndEmail> => {
+  const response = await fetch("/api/user");
+  const user = await response.json();
+  return user;
+};
+
+const example = async () => {
+  const user = await fetchUser();
+
+//   type test = Expect<Equal<typeof user, { name: string; email: string }>>;
+// Type 'false' does not satisfy the constraint 'true'.
+};
+
+// Exercise 2: Updating a Product
+
+interface Product1 {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+type ProductWithoutId = Partial<Omit<Product1, 'id'>>
+
+const updateProduct = (id: number, productInfo: ProductWithoutId) => {
+  // Do something with the productInfo
+};
